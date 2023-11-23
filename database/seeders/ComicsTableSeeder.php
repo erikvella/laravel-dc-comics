c67e6ef1ec164a30beba352b0975fadef308711e
erikvella <vella.erik1@gmail.com>
2023-11-23 18:20:07 +0100

    [add] seeder

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+use Illuminate\Support\Str;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comics');
+        foreach($comics as $comic){
+            $new_comic = new Comic();
+            $new_comic->title = $comic['title'];
+            $new_comic->slug = $this->generateSlug($new_comic->title);
+            $new_comic->description = $comic['description'];
+            $new_comic->thumb = $comic['thumb'];
+            $new_comic->price = $comic['price'];
+            $new_comic->series = $comic['series'];
+            $new_comic->sale_date = $comic['sale_date'];
+            $new_comic->type = $comic['type'];
+
+            dump($new_comic);
+
+        }
+    }
+
+    private function generateSlug($string){
+
+        $slug = Str::slug($string , '-');
+        $original_slug = $slug;
+
+        $exist = Comic::where('slug' , $slug)->first();
+        $c = 1;
+
+        while($exist){
+            $slug = $original_slug . '-' . $c;
+            $exist = Comic::where('slug' , $slug)->first();
+            $c++;
+        }
+        return $slug;
+
+    }
+}
